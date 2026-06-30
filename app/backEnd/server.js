d3c88c4a9f2a72e9f7a68c1a0aaa3bbe917d520e
hosnirabi <1hosnirabi@gmail.com>
2026-06-30 20:48:30 +0300

    first dockerfile

--- app/backEnd/server.js
+++ app/backEnd/server.js
@@ -15,12 +15,12 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/api/todo", toDoRoutes);
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
+  res.sendFile(path.join(__dirname, "../public", "about.html"));
 });
 
 const PORT = process.env.port;
